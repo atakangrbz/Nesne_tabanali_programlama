7afdcc04ff9c24eda1df9ba93d66120cbd63faa7
atakangrbz <atakan20043838@gmail.com>
2025-05-17 01:20:11 +0300

    Modül6_Ünite2

---
+++ Modül6_Ünite2/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(kareal(5));
+        Console.WriteLine(kareal(5.1));
+        Console.WriteLine(kareal("3"));
+
+
+    }
+    static int kareal(int x)
+    {
+        return x * x;
+    }
+    static double kareal(double x)
+    {
+        return x * x;
+    }
+    static int kareal(string x)
+    {
+        return int.Parse(x) * int.Parse(x);
+    }
+}
