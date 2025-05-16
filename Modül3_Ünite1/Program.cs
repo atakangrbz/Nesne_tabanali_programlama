cca30b76f9ba6a27eed18a99cdc82428a41dbecc
atakangrbz <atakan20043838@gmail.com>
2025-05-17 00:16:08 +0300

    Modül3_Ünite1

---
+++ Modül3_Ünite1/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 5;
+int b = 6;
+int c = 7;
+int islem = a + c / (b - c) * 10;
+int islemm = a + c / b - c * 10;
+int islemmm = a + c * b - c / 10;
+
+if (islem >= 0)
+    Console.WriteLine("Sonuç pozitiftir.");
+else
+    Console.WriteLine("Sonuç negatiftir.");
+if (islemm >= 0)
+    Console.WriteLine("Sonuç pozitiftir.");
+else
+    Console.WriteLine("Sonuç negatiftir.");
+if (islemmm >= 0)
+    Console.WriteLine("Sonuç pozitiftir.");
+else
+    Console.WriteLine("Sonuç negatiftir.");
