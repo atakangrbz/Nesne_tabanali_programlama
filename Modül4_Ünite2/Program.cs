28c758947442b723dacc7c9f850229a80a41fa5d
atakangrbz <atakan20043838@gmail.com>
2025-05-17 00:46:14 +0300

    Modül4_Ünite2

---
+++ Modül4_Ünite2/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void DegerArtir(ref int sayi)
+    {
+        sayi = sayi + 1; 
+    }
+
+    static void Main(string[] args)
+    {
+        int sayi = 5; 
+
+        Console.WriteLine("Önceki değer: " + sayi);
+
+        DegerArtir(ref sayi);
+
+        Console.WriteLine("Sonraki değer: " + sayi);
+
+    }
+}
