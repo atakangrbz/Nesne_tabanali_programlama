7d00657111e14dbc8de68519a50ddfb31b935544
atakangrbz <atakan20043838@gmail.com>
2025-05-17 00:41:40 +0300

    Modül4_Ünite1

---
+++ Modül4_Ünite1/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+
+    class Bisiklet
+    {
+        int hiz = 0;
+        int vites = 0;
+
+        public Bisiklet()
+        {
+            vites = 18;
+        }
+
+        public void Hizlan(int artis)
+        {
+            hiz += artis;
+        }
+
+        public void BilgileriYaz()
+        {
+            Console.WriteLine("Vites: {0}, Hız: {1}", vites, hiz);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Bisiklet bisiklet1 = new Bisiklet();
+        Bisiklet bisiklet2 = bisiklet1;
+        bisiklet1.Hizlan(10);
+        Console.WriteLine("bisiklet1 bilgileri:");
+        bisiklet1.BilgileriYaz();
+
+        Console.WriteLine("bisiklet2 bilgileri:");
+        bisiklet2.BilgileriYaz();
+
+
+    }
+}
