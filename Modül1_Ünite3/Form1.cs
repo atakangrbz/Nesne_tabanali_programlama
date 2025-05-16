5b8830ed33a87a711bde7278ab3422573c3bb5e4
atakangrbz <atakan20043838@gmail.com>
2025-05-16 23:43:29 +0300

    Modül1_Ünite3

---
+++ Modül1_Ünite3/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modül1_Ünite3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i = 30;
+            textBox1.Text = i.ToString();
+
+        }
+    }
+}
