0c9ea14bf0c5cebf00b912df4c2a6eb44c1dd595
atakangrbz <atakan20043838@gmail.com>
2025-05-16 23:59:30 +0300

    Modül2_Ünite2

---
+++ Modül2_Ünite2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+struct Tarih
+{
+    public int gun;
+    public int ay;
+    public int yil;
+    public Tarih(int g, int a, int y)
+    {
+        gun = g;
+        ay = a;
+        yil = y;
+    }
+    public void Yaz()
+    {
+        Console.WriteLine($"Tarih: {gun:D2}/{ay:D2}/{yil}");
+    }
+}
+class Program
+{
+    static void Main()
+    {        
+        Tarih bugun = new Tarih(01, 01, 2004);
+        bugun.Yaz();
+    }
+}
