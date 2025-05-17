ba49405cd88f1df6fb099970e0be3429e30b0547
atakangrbz <atakan20043838@gmail.com>
2025-05-17 14:46:37 +0300

    Modül7.Ünite1

---
+++ Modül7.Ünite1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Modül7.Ünite1;
+
+namespace Modül7.Ünite1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci birOgrenci = new Ogrenci("Ali UZUN");
+
+            Console.WriteLine("Adı");
+            Console.WriteLine("{0}", birOgrenci.Ad);
+
+            Console.WriteLine("Boyu");
+            Console.WriteLine("{0}", birOgrenci.Boy);
+
+            Console.WriteLine("Kilosu");
+            Console.WriteLine("{0}", birOgrenci.Kilo);
+
+            Console.WriteLine("\n");
+
+            Daire daire = new Daire(3.0);
+            Console.WriteLine("Dairenin Alanı:");
+            Console.WriteLine("{0}", daire.Alan());
+        }
+    }
+}
